84232da76717c68173ff7c945050fc3dcf040794
Michael Bishoff <bishoff1@umbc.edu>
2015-03-28 20:34:16 -0400

    remove edwin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/edwin', routes.edwin);
+//app.get('/edwin', routes.edwin);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
