ae38409f645965bd9b150fc0b7563c397483c0da
Michael Bishoff <bishoff1@umbc.edu>
2015-03-28 20:24:42 -0400

    added users thing

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/edwin', routes.edwin);
 app.get('/users', user.list);
+app.get('/edwin', routes.edwin);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
