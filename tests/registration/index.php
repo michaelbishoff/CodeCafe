b3320c321a8345aa312d78d9a939e06b134d6c0f
CelestineW <cwong2@umbc.edu>
2015-03-29 07:59:26 -0400

    added stylesheets and registration pg

---
+++ tests/registration/index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id = "home-body">
+            <form method = "post" action = "index.html">
+            
+            <div class="user-name" id="wrap">
+                <div id="name">
+                    Name
+                </div>
+                <input type ="text" id="u_name">
+            </div>
+            
+            <div class="user-alias" id="wrap">
+                <div id="name">
+                    </div></div></form></div>
+    
+    
+    
+    </body>
+</html>
