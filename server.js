840917aa2e48c2094538df27affd09a08adbba93
Michael Bishoff <bishoff1@umbc.edu>
2015-03-29 10:07:49 -0400

    no modlue.exports

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/', routes);
 //app.get('/registration', registration);
 //app.get('/users', user.list);
 
-module.exports = app;
+//module.exports = app;
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
