ca6ecc2d1a6099b4503a49997517cd6ccacd240d
Michael Bishoff <bishoff1@umbc.edu>
2015-03-28 20:27:31 -0400

    trying edwins

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Code Cafe' });
 };
+
+/*
+ * GET Edwin's page.
+ */
+
+exports.edwin = function(req, res){
+    res.render('edwin', { });
+};
