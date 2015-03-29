319060750b0e134ff3b08b1a23b463f3701e8e81
Michael Bishoff <bishoff1@umbc.edu>
2015-03-29 12:17:04 -0400

    Added mikes stuff

--- routes/cafe.js
+++ routes/cafe.js
@@ -2,6 +2,25 @@
  * GET home page.
  */
 exports.index = function(req, res){
-//    res.sendfile('/views/index.html');
   res.render('cafe', { });
+};
+
+exports.post = function(req, res){
+  var python = require('child_process').spawn(
+    'python',
+    // second argument is array of parameters, e.g.:                       
+     ["public/scripts/execute.py"]
+     );
+
+ var output = "";
+ python.stdout.on('data', function(data){ output += data });
+ python.on('close', function(code){
+     if (code !== 0) {  return res.send(500, code); }
+
+ // Splits the output into compiler errors, stdout, and stderr          
+ output = output.split('~');
+
+ res.render('run', { title: "Run", code: output[0], compileInfo: output[1], stdout: output[2], stderr: output[3] });
+  });
+
 };
